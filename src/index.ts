82acdb4268d27a1640449ad99649d0c385643c1f
pmhxhsj <minho7952@naver.com>
2023-04-20 21:51:46 +0900

    Icon 컴포넌트 삭제 및 input, label 모듈화

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Button } from "./components/Button/Button";
-export { default as ButtonGroup } from "./components/ButtonGroup/ButtonGroup";
-export { default as Icon } from "./Icon/Icon";
+export { default as Input } from "./components/Input/Input";
+export { default as Label } from "./components/Label/Label";
 export { default as Typography } from "./components/Typography/Typography";
 export { default as FlexContainer } from "./components/FlexContainer/FlexContainer";
