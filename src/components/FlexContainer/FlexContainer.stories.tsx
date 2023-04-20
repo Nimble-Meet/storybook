4479f4a1624cd22e25ac455b9d5490c72516de33
pmhxhsj <minho7952@naver.com>
2023-04-20 21:47:28 +0900

    feat: FlexContainer 스토리 정의

--- src/components/FlexContainer/FlexContainer.stories.tsx
+++ src/components/FlexContainer/FlexContainer.stories.tsx
@@ -1,3 +1,28 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import FlexContainer from "./FlexContainer";
 
-import { ComponentStory } from "@storybook/react";
+export default {
+  title: "FlexContainer",
+  component: FlexContainer,
+} as ComponentMeta<typeof FlexContainer>;
+
+const Template: ComponentStory<typeof FlexContainer> = (args) => (
+  <FlexContainer {...args} />
+);
+
+export const BasicFlexContainer = Template.bind({});
+
+BasicFlexContainer.args = {
+  direction: "column",
+  justifyContent: "start",
+  alignItems: "start",
+  gap: "0.5rem",
+  children: (
+    <>
+      <div>Box1</div>
+      <div>Box2</div>
+      <div>Box3</div>
+    </>
+  ),
+};
